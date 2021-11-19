402dc4c5533349cfd2bbef90c74d57c0b81562c7
sena-anny <cozykoji18@gmail.com>
2021-11-19 22:50:31 +0900

    go routine practice

---
+++ go-practice/go-routine/main.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+)
+
+func main() {
+	start := time.Now()
+
+	apis := []string{
+		"https://management.azure.com",
+		"https://dev.azure.com",
+		"https://api.github.com",
+		"https://outlook.office.com/",
+		"https://api.somewhereintheinternet.com/",
+		"https://graph.microsoft.com",
+	}
+
+	ch := make(chan string, 10)
+
+	ch1 := make(chan string)
+	ch2 := make(chan string)
+
+	go process(ch1)
+	go replicate(ch2)
+
+	for i := 0; i < 2; i++ {
+		select {
+		case process := <-ch1:
+			fmt.Printf(process)
+		case replicate := <-ch2:
+			fmt.Printf(replicate)
+		}
+	}
+
+	for _, api := range apis {
+		go checkAPI(api, ch)
+	}
+
+	for i := 0; i < len(apis); i++ {
+		fmt.Print(<-ch)
+	}
+
+	elapsed := time.Since(start)
+	fmt.Printf("Done! It took %v seconds\n", elapsed.Seconds())
+}
+
+func checkAPI(api string, ch chan string) {
+	_, err := http.Get(api)
+	if err != nil {
+		ch <- fmt.Sprintf("ERROR: %s is down \n", api)
+		return
+	}
+	ch <- fmt.Sprintf("Success: %s is up and running\n", api)
+}
+
+func replicate(ch chan string) {
+	time.Sleep(1 * time.Second)
+	ch <- "Done replicating!"
+}
+
+func process(ch chan string) {
+	time.Sleep(3 * time.Second)
+	ch <- "Done process!!!"
+}
