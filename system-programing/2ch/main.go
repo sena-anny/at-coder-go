69c927990dcd0fed2e54f7c2389d02c580900ddd
sena-anny <cozykoji18@gmail.com>
2022-01-01 18:06:45 +0900

    system programing 2ch

--- system-programing/2ch/main.go
+++ system-programing/2ch/main.go
@@ -1,13 +1,30 @@
 package main
 
-import "net/http"
+import (
+	"compress/gzip"
+	"encoding/json"
+	"io"
+	"log"
+	"net/http"
+	"os"
+)
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Encoding", "gzip")
 	w.Header().Set("Content-Type", "application/json")
-	//source := map[string]string{
-	//	"Hello": "World",
-	//}
+	source := map[string]string{
+		"Hello": "World",
+	}
+	g := gzip.NewWriter(w)
+	out := io.MultiWriter(g, os.Stdout)
+	encoder := json.NewEncoder(out)
+	encoder.SetIndent("", "    ")
+	if err := encoder.Encode(source); err != nil {
+		log.Print(err)
+	}
+	if err := g.Flush(); err != nil {
+		log.Print(err)
+	}
 }
 
 func main() {
