e975f845e1b6390b4625211533ecbbc0e2f5f0c7
sena-anny <cozykoji18@gmail.com>
2022-01-26 23:12:11 +0900

    探索問題演習

---
+++ search-practice/MSB2019D/main.go
@@ -0,0 +1,101 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"path/filepath"
+	"strconv"
+)
+
+var customIo *CustomIo
+
+type CustomIo struct {
+	Scanner *bufio.Scanner
+	Writer  *bufio.Writer
+}
+
+func NewCustomIo(in io.Reader, out io.Writer) *CustomIo {
+	const BufSize = 2000005
+	scanner := bufio.NewScanner(in)
+	scanner.Split(bufio.ScanWords)
+	scanner.Buffer(make([]byte, BufSize), BufSize)
+	return &CustomIo{Scanner: scanner, Writer: bufio.NewWriter(out)}
+}
+
+func (i *CustomIo) Text() string {
+	if !i.Scanner.Scan() {
+		panic("scan failed")
+	}
+	return i.Scanner.Text()
+}
+func (i *CustomIo) Atoi(s string) int                 { x, _ := strconv.Atoi(s); return x }
+func (i *CustomIo) GetNextInt() int                   { return i.Atoi(i.Text()) }
+func (i *CustomIo) Atoi64(s string) int64             { x, _ := strconv.ParseInt(s, 10, 64); return x }
+func (i *CustomIo) GetNextInt64() int64               { return i.Atoi64(i.Text()) }
+func (i *CustomIo) Atof64(s string) float64           { x, _ := strconv.ParseFloat(s, 64); return x }
+func (i *CustomIo) GetNextFloat64() float64           { return i.Atof64(i.Text()) }
+func (i *CustomIo) Print(x ...interface{})            { fmt.Fprint(i.Writer, x...) }
+func (i *CustomIo) Printf(s string, x ...interface{}) { fmt.Fprintf(i.Writer, s, x...) }
+func (i *CustomIo) Println(x ...interface{})          { fmt.Fprintln(i.Writer, x...) }
+func isLocal() bool                                   { return os.Getenv("AT_CODER") == "KJ" }
+
+func main() {
+	in := os.Stdin
+	out := os.Stdout
+
+	if isLocal() {
+		path, err := filepath.Abs("./input.txt")
+		in, err = os.Open(path)
+		if err != nil {
+			log.Printf("ファイル読み込み失敗:%v", err)
+		}
+	}
+
+	customIo = NewCustomIo(in, out)
+	defer customIo.Writer.Flush()
+	solve()
+}
+
+func solve() {
+	N := customIo.GetNextInt()
+	S := customIo.Text()
+
+	ans := 0
+	for i := 0; i < 1000; i++ {
+		V := fmt.Sprintf("%03d", i)
+		count := 0
+		for l := 0; l < N; l++ {
+			if count >= 3 {
+				break
+			}
+			if S[l] != V[count] {
+				continue
+			}
+			count++
+		}
+		if count >= 3 {
+			ans++
+		}
+	}
+	customIo.Println(ans)
+
+}
+
+func cntDigits(n int) int {
+	digits := 0
+	for n > 0 {
+		n = n / 10
+		digits++
+	}
+	return digits
+}
+
+func maxInt(x int, y int) int {
+	if x >= y {
+		return x
+	}
+	return y
+}
