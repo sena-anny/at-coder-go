577b1acbbd04b4d949dca0717cc2b3d7f8dc0bb4
sena-anny <cozykoji18@gmail.com>
2022-02-20 02:24:57 +0900

    ABC239

---
+++ ABC239/C-problem/main.go
@@ -0,0 +1,115 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"log"
+	"math"
+	"os"
+	"path/filepath"
+	"strconv"
+)
+
+var customIo *CustomIo
+
+type CustomIo struct {
+	Scanner *bufio.Scanner
+	Writer  *bufio.Writer
+}
+
+func NewCustomIo(in io.Reader, out io.Writer) *CustomIo {
+	const BufSize = 2000005
+	scanner := bufio.NewScanner(in)
+	scanner.Split(bufio.ScanWords)
+	scanner.Buffer(make([]byte, BufSize), BufSize)
+	return &CustomIo{Scanner: scanner, Writer: bufio.NewWriter(out)}
+}
+
+func (i *CustomIo) Text() string {
+	if !i.Scanner.Scan() {
+		panic("scan failed")
+	}
+	return i.Scanner.Text()
+}
+func (i *CustomIo) Atoi(s string) int                 { x, _ := strconv.Atoi(s); return x }
+func (i *CustomIo) GetNextInt() int                   { return i.Atoi(i.Text()) }
+func (i *CustomIo) Atoi64(s string) int64             { x, _ := strconv.ParseInt(s, 10, 64); return x }
+func (i *CustomIo) GetNextInt64() int64               { return i.Atoi64(i.Text()) }
+func (i *CustomIo) Atof64(s string) float64           { x, _ := strconv.ParseFloat(s, 64); return x }
+func (i *CustomIo) GetNextFloat64() float64           { return i.Atof64(i.Text()) }
+func (i *CustomIo) Print(x ...interface{})            { fmt.Fprint(i.Writer, x...) }
+func (i *CustomIo) Printf(s string, x ...interface{}) { fmt.Fprintf(i.Writer, s, x...) }
+func (i *CustomIo) Println(x ...interface{})          { fmt.Fprintln(i.Writer, x...) }
+func isLocal() bool                                   { return os.Getenv("AT_CODER") == "KJ" }
+
+func main() {
+	in := os.Stdin
+	out := os.Stdout
+
+	if isLocal() {
+		path, err := filepath.Abs("./input.txt")
+		in, err = os.Open(path)
+		if err != nil {
+			log.Printf("ファイル読み込み失敗:%v", err)
+		}
+	}
+
+	customIo = NewCustomIo(in, out)
+	defer customIo.Writer.Flush()
+	solve()
+}
+
+func solve() {
+	X1 := customIo.GetNextInt()
+	Y1 := customIo.GetNextInt()
+	X2 := customIo.GetNextInt()
+	Y2 := customIo.GetNextInt()
+	DX := X1 - X2
+	DY := Y1 - Y2
+	Distance := math.Sqrt(float64(DX*DX + DY*DY))
+
+	ans := "No"
+	if Distance == 2 || Distance == 4 || Distance == math.Sqrt(2) || Distance == math.Sqrt(10) || Distance == math.Sqrt(18) || Distance == math.Sqrt(20) {
+		ans = "Yes"
+	}
+	customIo.Println(ans)
+
+}
+
+func cntDigits(n int) int {
+	digits := 0
+	for n > 0 {
+		n = n / 10
+		digits++
+	}
+	return digits
+}
+
+func maxInt(x, y int) int {
+	if x >= y {
+		return x
+	}
+	return y
+}
+
+func minInt(x, y int) int {
+	if x < y {
+		return x
+	}
+	return y
+}
+
+// mod Mにおけるaの逆元を求める（非再帰拡張 Euclid の互除法）
+func invMod(a, M int) int {
+	p, x, u := M, 1, 0
+	for p != 0 {
+		t := a / p
+		a, p = p, a-t*p
+		x, u = u, x-t*u
+	}
+	if x < 0 {
+		x += M
+	}
+	return x
+}
