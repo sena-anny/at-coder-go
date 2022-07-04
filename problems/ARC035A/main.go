c2bbaf14a72ccfec90c075cbb33d6096fbb00bbb
sena-anny <cozykoji18@gmail.com>
2022-07-04 23:59:41 +0900

    ARC035A

---
+++ problems/ARC035A/main.go
@@ -0,0 +1,221 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"log"
+	"math"
+	"os"
+	"path/filepath"
+	"sort"
+	"strconv"
+)
+
+var customIo *CustomIo
+
+type CustomIo struct {
+	Scanner *bufio.Scanner
+	Writer  *bufio.Writer
+}
+
+func NewCustomIo(in io.Reader, out io.Writer) *CustomIo {
+	const BufSize = 2000005
+	scanner := bufio.NewScanner(in)
+	scanner.Split(bufio.ScanWords)
+	scanner.Buffer(make([]byte, BufSize), BufSize)
+	return &CustomIo{Scanner: scanner, Writer: bufio.NewWriter(out)}
+}
+
+func (i *CustomIo) Text() string {
+	if !i.Scanner.Scan() {
+		panic("scan failed")
+	}
+	return i.Scanner.Text()
+}
+func (i *CustomIo) Atoi(s string) int                 { x, _ := strconv.Atoi(s); return x }
+func (i *CustomIo) GetNextInt() int                   { return i.Atoi(i.Text()) }
+func (i *CustomIo) Atoi64(s string) int64             { x, _ := strconv.ParseInt(s, 10, 64); return x }
+func (i *CustomIo) GetNextInt64() int64               { return i.Atoi64(i.Text()) }
+func (i *CustomIo) Atof64(s string) float64           { x, _ := strconv.ParseFloat(s, 64); return x }
+func (i *CustomIo) GetNextFloat64() float64           { return i.Atof64(i.Text()) }
+func (i *CustomIo) Print(x ...interface{})            { fmt.Fprint(i.Writer, x...) }
+func (i *CustomIo) Printf(s string, x ...interface{}) { fmt.Fprintf(i.Writer, s, x...) }
+func (i *CustomIo) Println(x ...interface{})          { fmt.Fprintln(i.Writer, x...) }
+func isLocal() bool                                   { return os.Getenv("AT_CODER") == "KJ" }
+
+func main() {
+	in := os.Stdin
+	out := os.Stdout
+
+	if isLocal() {
+		path, err := filepath.Abs("./input.txt")
+		in, err = os.Open(path)
+		if err != nil {
+			log.Printf("ファイル読み込み失敗:%v", err)
+		}
+	}
+
+	customIo = NewCustomIo(in, out)
+	defer func() {
+		customIo.Writer.Flush()
+	}()
+	solve()
+}
+
+func solve() {
+	s := customIo.Text()
+	num := len(s)
+	ans := false
+	for i := 0; i < num/2; i++ {
+		if s[i] == s[num-1-i] || s[i] == '*' || s[num-1-i] == '*' {
+			ans = true
+		} else {
+			ans = false
+			break
+		}
+	}
+	if ans {
+		customIo.Println("YES")
+	} else {
+		customIo.Println("NO")
+	}
+}
+
+func cntDigits(n int) int {
+	digits := 0
+	for n > 0 {
+		n = n / 10
+		digits++
+	}
+	return digits
+}
+
+func maxInt(x, y int) int {
+	if x >= y {
+		return x
+	}
+	return y
+}
+
+func minInt(x, y int) int {
+	if x < y {
+		return x
+	}
+	return y
+}
+
+func absInt(x int) int {
+	if x < 0 {
+		return -x
+	}
+	return x
+}
+
+// mod Mにおけるaの逆元を求める（非再帰拡張 Euclid の互除法）
+func invMod(a, M int) int {
+	p, x, u := M, 1, 0
+	for p != 0 {
+		t := a / p
+		a, p = p, a-t*p
+		x, u = u, x-t*u
+	}
+	if x < 0 {
+		x += M
+	}
+	return x
+}
+
+// 順列 nPk
+func permutation(N, K int) int {
+	v := 1
+	if 0 < K && K <= N {
+		for i := 0; i < K; i++ {
+			v *= N - i
+		}
+	} else if K > N {
+		v = 0
+	}
+	return v
+}
+
+/*
+    順列全探索
+	for {
+		// Do something
+		if !nextPermutation(sort.IntSlice(x)) {
+			break
+		}
+	}
+*/
+func nextPermutation(x sort.Interface) bool {
+	n := x.Len() - 1
+	if n < 1 {
+		return false
+	}
+	j := n - 1
+	for ; !x.Less(j, j+1); j-- {
+		if j == 0 {
+			return false
+		}
+	}
+	l := n
+	for !x.Less(j, l) {
+		l--
+	}
+	x.Swap(j, l)
+	for k, l := j+1, n; k < l; {
+		x.Swap(k, l)
+		k++
+		l--
+	}
+	return true
+}
+
+// ２つのスライスの値を比較
+func equalSlice(a, b []int) bool {
+	if len(a) != len(b) {
+		return false
+	}
+
+	for i := 0; i < len(a); i++ {
+		if a[i] != b[i] {
+			return false
+		}
+	}
+
+	return true
+}
+
+// Point is int point
+type point struct {
+	x int
+	y int
+}
+
+// 2点(a-b)間の距離
+func pointDistance(a, b point) float64 {
+	return math.Sqrt(float64((a.x-b.x)*(a.x-b.x) + (a.y-b.y)*(a.y-b.y)))
+}
+
+/*
+	二分探索
+	[ok,ng]でjudge関数でtrueを返却する最大の値を返却する
+	該当しない場合はok値が返却される
+    例)
+    x := 5
+    // sort済みスライスが存在する前提
+	bs(0, max, func(i int) bool { return data[i] > x // true or false })
+*/
+func bs(ok, ng int, judge func(int) bool) int {
+	for absInt(ok-ng) > 1 {
+		mid := (ok + ng) / 2
+
+		if judge(mid) {
+			ok = mid
+		} else {
+			ng = mid
+		}
+	}
+	return ok
+}
